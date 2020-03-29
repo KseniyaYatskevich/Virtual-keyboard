6834afa58e390039c6df21901225390b3e66754f
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-03-29 19:49:29 +0300

    init: start project,add  webpack configuration

---
+++ Virtual-keyboard/webpack.config.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+    const isProduction = options.mode === 'production';
+
+    const config = {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'source-map',
+        watch: !isProduction,
+        entry: ['./src/index.js', './src/style.css'],
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'script.js',
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: ['@babel/preset-env']
+                      }
+                    }
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,                    
+                        'css-loader',                        
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpe?g|gif)$/i,
+                    use: [
+                      {
+                        loader: 'file-loader',
+                      },
+                    ],
+                },
+                {
+                    test: /\.html$/i,
+                    loader: 'html-loader',
+                },
+            ]
+        },
+
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+            })
+        ]
+    };
+
+
+    return config;
+}
